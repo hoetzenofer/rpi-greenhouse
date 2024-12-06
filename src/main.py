e200ee39385687d19c31d3722839d945ad5aa45e
hoetzenofer <felixhotzendorfer@gmail.com>
2024-12-07 00:30:41 +0100

    Add ON-OFF switch

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
 # main.py
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+pin = 7
+GPIO.setup(pin, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(pin, GPIO.HIGH)
+        print("Pin eingeschaltet")
+        time.sleep(5)
+        
+        GPIO.output(pin, GPIO.LOW)
+        print("Pin ausgeschaltet")
+        time.sleep(5)
+
+except KeyboardInterrupt:
+    print("Programm beendet")
+
+finally:
+    GPIO.cleanup()
+
