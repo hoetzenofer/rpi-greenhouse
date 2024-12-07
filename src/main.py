862240f3859dc342a41578b727e0b344f294e8e7
hoetzenofer <felixhotzendorfer@gmail.com>
2024-12-07 01:03:54 +0100

    Add function for GPIO controlling

--- src/main.py
+++ src/main.py
@@ -1,26 +1,13 @@
 # main.py
 
 import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BCM)
 
-pin = 7
-GPIO.setup(pin, GPIO.OUT)
+def set_state(pin: int, state: bool):
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH if state == True else GPIO.LOW)
 
-try:
-    while True:
-        GPIO.output(pin, GPIO.HIGH)
-        print("Pin eingeschaltet")
-        time.sleep(5)
-        
-        GPIO.output(pin, GPIO.LOW)
-        print("Pin ausgeschaltet")
-        time.sleep(5)
-
-except KeyboardInterrupt:
-    print("Programm beendet")
-
-finally:
     GPIO.cleanup()
 
+set_state(17, True)
